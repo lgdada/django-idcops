cfd57b83567746288e9af909557b1eb388fcaf9e
Wenvki <294060408@qq.com>
2021-04-08 17:47:05 +0800

    fix import online create date is none value error

--- idcops/imports.py
+++ idcops/imports.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import date
 
 import re
 import xlrd
@@ -43,7 +44,10 @@ def import_online(path, onidc_id):
             continue
         data = dict(zip(headers, [k.value for k in row]))
         raw = {k: data.get(k) for k in fileds}
-        created = datetime.strptime(data.get('created'), '%Y-%m-%d')
+        try:
+            created = datetime.strptime(data.get('created'), '%Y-%m-%d')
+        except BaseException:
+            created = datetime.now().date().strftime('%Y-%m-%d')
         raw.update(**dict(
             created=created,
             sn=force_text(data.get('sn'))
